d2b576a408a132c135b76149c6546bcdcbdefd34
David Smith <david.smith@wirewool.com>
2017-12-28 12:09:09 +0000

    Added in the location as a argument

--- enviro-json-output.py
+++ enviro-json-output.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
+import argparse
 import json
 from datetime import datetime
 from envirophat import light, motion, weather, leds
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--location',
+                    default='Unkown',
+                    help='Location to descibe where the Pi is')
+args = parser.parse_args()
 
 leds.on()
 rgb = str(light.rgb())[1:-1].replace(' ', '')
@@ -11,6 +17,8 @@ leds.off()
 acc = str(motion.accelerometer())[1:-1].replace(' ', '')
 
 data = {
+    'location': args.location,
+    'time': datetime.utcnow().isoformat(),
     'light': light.light(),
     'rgb': rgb,
     'motion': acc,
