fdecd33e3e8cdc8f50cc9f23df1ef2e8be568bb8
David Smith <david.smith@wirewool.com>
2017-12-28 18:06:04 +0000

    Updated to use values from .env and also send data to mongo - debug is WIP

--- enviro-json-output.py
+++ enviro-json-output.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 import argparse
 import json
 from datetime import datetime
 from envirophat import light, motion, weather, leds
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DEBUG = os.getenv('DEBUG', None)
+MONGO_DB_URL = os.getenv('MONGO_DB')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--location',
@@ -27,4 +39,21 @@ data = {
     'pressure': weather.pressure(),
 }
 
-print(json.dumps(data))
+# json_data = json.dumps(data)
+# print(json_data)
+
+client = MongoClient(MONGO_DB_URL)
+
+try:
+    result = client.admin.command("ismaster")
+    print("Connected to Mongo")
+except ConnectionFailure as e:
+    print(e)
+
+db = client.test
+enviro = db.enviro
+id = enviro.insert_one(data).inserted_id
+
+print(id)
+
+client.close()
